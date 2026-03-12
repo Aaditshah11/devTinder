30e2c675688d92f231909e1d05addc13447d31be
Aaditshah11 <aaditshah2006@gmail.com>
2026-03-12 22:34:34 +0530

    validator package add

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -22,12 +23,19 @@ const userSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
       trim: true,
-    },
 
+      validate(value) {
+        if (!validator.isEmail(value)) throw new Error("Enter valid email");
+      },
+    },
     password: {
       type: String,
       required: true,
       minlength: 6,
+      validate(value) {
+        if (!validator.isStrongPassword(value))
+          throw new Error("Enter strong password");
+      },
     },
 
     age: {
@@ -48,6 +56,10 @@ const userSchema = new mongoose.Schema(
     photoUrl: {
       type: String,
       default: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+
+      validate(value) {
+        if (!validator.isURL(value)) throw new Error("Enter valid URL");
+      },
     },
 
     about: {
