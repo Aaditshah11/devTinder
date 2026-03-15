92982231fa1b705854a03848d464b89e71c3d0f5
Aaditshah11 <aaditshah2006@gmail.com>
2026-03-15 22:19:22 +0530

    user feed

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const connectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter = express.Router();
+
+const SAFE_FIELDS = ["firstName", "lastName", "photoUrl", "about"];
 
 userRouter.get("/user/requests", userAuth, async (req, res) => {
   try {
@@ -12,7 +15,7 @@ userRouter.get("/user/requests", userAuth, async (req, res) => {
         toUserId: loggedInUser._id,
         status: "interested",
       })
-      .populate("fromUserId", ["firstName", "lastName", "photoUrl", "about"]);
+      .populate("fromUserId", SAFE_FIELDS);
 
     if (!connectionRequests)
       return res.status(404).json({ message: "no connection requests" });
@@ -32,8 +35,8 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
         status: "accepted",
       })
-      .populate("fromUserId", ["firstName", "lastName", "photoUrl", "about"])
-      .populate("toUserId", ["firstName", "lastName", "photoUrl", "about"]);
+      .populate("fromUserId", SAFE_FIELDS)
+      .populate("toUserId", SAFE_FIELDS);
 
     console.log(connections);
 
@@ -44,6 +47,36 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     });
 
     res.json({ message: "All connections", data });
+  } catch (err) {
+    res.status(500).send("Error: " + err.message);
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const skip = (page - 1) * limit || 0;
+
+    const loggedInUser = req.user;
+
+    const existingConnections = await connectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    });
+
+    const excludedUserIds = existingConnections.map((conn) => {
+      return conn.fromUserId.equals(loggedInUser._id)
+        ? conn.toUserId
+        : conn.fromUserId;
+    });
+
+    const allExcludedIds = [...excludedUserIds, loggedInUser._id];
+
+    const feed = await User.find({ _id: { $nin: allExcludedIds } })
+      .select(SAFE_FIELDS)
+      .skip(skip)
+      .limit(limit);
+    res.json({ message: "User feed fetched successfully", data: feed });
   } catch (err) {
     res.status(500).send("Error: " + err.message);
   }
