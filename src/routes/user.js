ba727b4f156582e5e1d1d45adcca6d7a52e5c8eb
Aaditshah11 <aaditshah2006@gmail.com>
2026-03-15 19:06:39 +0530

    view connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -19,10 +19,34 @@ userRouter.get("/user/requests", userAuth, async (req, res) => {
 
     res.json({ message: "All connection requests", data: connectionRequests });
   } catch (err) {
-    res.status(400).send("Error: " + err.message);
+    res.status(500).send("Error: " + err.message);
   }
 });
 
-userRouter.get("/user/connections", userAuth, async (req, res) => {});
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connections = await connectionRequest
+      .find({
+        $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+        status: "accepted",
+      })
+      .populate("fromUserId", ["firstName", "lastName", "photoUrl", "about"])
+      .populate("toUserId", ["firstName", "lastName", "photoUrl", "about"]);
+
+    console.log(connections);
+
+    const data = connections.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString())
+        return row.toUserId;
+      else return row.fromUserId;
+    });
+
+    res.json({ message: "All connections", data });
+  } catch (err) {
+    res.status(500).send("Error: " + err.message);
+  }
+});
 
 module.exports = userRouter;
